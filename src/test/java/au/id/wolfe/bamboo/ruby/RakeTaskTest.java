15a7512d9ed57e5fde2b9202c5d3cf55458f529e
Mark Wolfe <mark@wolfe.id.au>
2011-11-10 15:12:25 +1100

    Support multiple tasks.

---
+++ src/test/java/au/id/wolfe/bamboo/ruby/RakeTaskTest.java
@@ -0,0 +1,17 @@
+package au.id.wolfe.bamboo.ruby;
+
+import org.junit.Before;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: markw
+ * Date: 10/11/11
+ * Time: 2:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RakeTaskTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
