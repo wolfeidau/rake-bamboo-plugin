46cc5e80168da5aeb0c321dbd14524d9789abefd
Mark Wolfe <mark@wolfe.id.au>
2013-02-08 09:53:03 +1100

    Some documentation and updating to bamboo 4.4

--- src/main/java/au/id/wolfe/bamboo/ruby/util/ExecHelper.java
+++ src/main/java/au/id/wolfe/bamboo/ruby/util/ExecHelper.java
@@ -8,19 +8,29 @@ import java.io.IOException;
 import static au.id.wolfe.bamboo.ruby.util.ExecUtils.cmdExec;
 
 /**
- *
+ * Exec Helper which is used mainly on windows to locate the ruby executable.
  */
 public class ExecHelper {
 
     private static final Logger log = LoggerFactory.getLogger(ExecHelper.class);
 
-    public String getExecutablePath(String locatorCommand, String executable, boolean multiline) throws IOException, InterruptedException {
+    /**
+     * This routine uses an older method for locating exe files in the path and is compatible with xp.
+     *
+     * @param locatorCommand The command used to locate an executable.
+     * @param executable     The command to locate on the path.
+     * @param multiLine      Whether or not multi line output should be returned.
+     * @return The output returned when the command was run.
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public String getExecutablePath(String locatorCommand, String executable, boolean multiLine) throws IOException, InterruptedException {
 
         final String line = locatorCommand + " " + executable;
 
         final StringBuffer output = new StringBuffer();
 
-        final int exitValue = cmdExec(line, output, multiline);
+        final int exitValue = cmdExec(line, output, multiLine);
 
         log.info("where exit code  = {}", exitValue);
 
@@ -33,20 +43,21 @@ public class ExecHelper {
     }
 
     /**
-     * This routine uses an older method for locatating exe files in the path and is compatible with xp.
+     * This routine uses an older method for locating exe files in the path and is compatible with xp.
+     *
      * @param executable The command to locate on the path.
-     * @param multiline Whether or not multi line output should be returned.
+     * @param multiLine  Whether or not multi line output should be returned.
      * @return The output returned when the command was run.
      * @throws IOException
      * @throws InterruptedException
      */
-    public String getExecutablePath(String executable, boolean multiline) throws IOException, InterruptedException {
+    public String getExecutablePath(String executable, boolean multiLine) throws IOException, InterruptedException {
 
         final String line = "cmd.exe /c for %i in (" + executable + ") do @echo. %~$PATH:i";
 
         final StringBuffer output = new StringBuffer();
 
-        final int exitValue = cmdExec(line, output, multiline);
+        final int exitValue = cmdExec(line, output, multiLine);
 
         log.info("where exit code  = {}", exitValue);
 
