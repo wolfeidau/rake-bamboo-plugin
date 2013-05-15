d6517f151150cc0f902b512b214cee91cd11b458
Mark Wolfe <mark@wolfe.id.au>
2013-05-15 19:23:52 +1000

    Fix for issue #37 fixed silly logic issue which tripped with 2.0 ruby.

--- src/main/java/au/id/wolfe/bamboo/ruby/system/SystemRubyUtils.java
+++ src/main/java/au/id/wolfe/bamboo/ruby/system/SystemRubyUtils.java
@@ -11,25 +11,26 @@ public final class SystemRubyUtils {
 
     static final Pattern ruby18pattern = Pattern.compile("ruby (\\d\\.\\d\\.\\d) \\((.*) patchlevel (\\d+)\\) \\[(.*)\\]");
     static final Pattern ruby19pattern = Pattern.compile("ruby (\\d+\\.\\d+\\.\\d)+p(\\d+) \\((.*)\\) \\[(.*)\\]");
+    static final Pattern ruby20pattern = Pattern.compile("ruby (\\d+\\.\\d+\\.\\d)+p(\\d+) \\((.*)\\) \\[(.*)\\]");
 
     public static String parseRubyVersionString(String rubyVersionString) {
 
-        if (rubyVersionString.contains("1.9")) {
+        if (rubyVersionString.contains("1.8")) {
 
-            Matcher matcher = ruby19pattern.matcher(rubyVersionString);
+            Matcher matcher = ruby18pattern.matcher(rubyVersionString);
 
             if (matcher.matches() && matcher.groupCount() == 4) {
-                return String.format("%s-p%s", matcher.group(1), matcher.group(2));
+                return String.format("%s-p%s", matcher.group(1), matcher.group(3));
             } else {
                 throw new IllegalArgumentException("Unable to parse ruby version string.");
             }
 
         } else {
 
-            Matcher matcher = ruby18pattern.matcher(rubyVersionString);
+            Matcher matcher = ruby19pattern.matcher(rubyVersionString);
 
             if (matcher.matches() && matcher.groupCount() == 4) {
-                return String.format("%s-p%s", matcher.group(1), matcher.group(3));
+                return String.format("%s-p%s", matcher.group(1), matcher.group(2));
             } else {
                 throw new IllegalArgumentException("Unable to parse ruby version string.");
             }
--- src/test/java/au/id/wolfe/bamboo/ruby/system/SystemRubyUtilsTest.java
+++ src/test/java/au/id/wolfe/bamboo/ruby/system/SystemRubyUtilsTest.java
@@ -13,6 +13,9 @@ public class SystemRubyUtilsTest {
     static final String rubyOsxVersionString  =
             "ruby 1.8.7 (2010-01-10 patchlevel 249) [universal-darwin11.0]";
 
+    static final String rubyLinuxVersionString  =
+            "ruby 2.0.0p0 (2013-02-24 revision 39474) [x86_64-linux]";
+
     static final String rubyWindowsVersionString = "ruby 1.9.3p125 (2012-02-16) [i386-mingw32]";
 
     @Test
@@ -21,6 +24,14 @@ public class SystemRubyUtilsTest {
         String rubyVersion =  SystemRubyUtils.parseRubyVersionString(rubyOsxVersionString);
 
         assertThat(rubyVersion, equalTo("1.8.7-p249"));
+    }
+
+    @Test
+    public void testParseLinuxRubyVersionString() throws Exception {
+
+        String rubyVersion =  SystemRubyUtils.parseRubyVersionString(rubyLinuxVersionString);
+
+        assertThat(rubyVersion, equalTo("2.0.0-p0"));
     }
 
     @Test
